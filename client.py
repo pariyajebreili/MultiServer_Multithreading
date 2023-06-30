d99ce3826e71b4fa9aa10c9c887c0509508a6435
Pariya <pariyajebreili@gmail.com>
2023-06-30 10:49:59 +0330

    nearly compelted

--- client.py
+++ client.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Rest of the code goes here
 
-def handle_client(client_socket, q):
+def handle_client(client_socket):
     # Send a message to the server
     msg = 'Hello, server!'
     client_socket.send(msg.encode(FORMAT))
@@ -33,7 +33,7 @@ def handle_client(client_socket, q):
     client_socket.send(msg.encode(FORMAT))
 
     # Remove the client from the queue
-    q.get_nowait()
+    #q.get_nowait()
     client_socket.close()
     logging.info("[DISCONNECTED] Client disconnected.")
 
@@ -42,11 +42,12 @@ def handle_client(client_socket, q):
 def start():
     # Create a thread pool of NUM_CONNECTIONS threads
     with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_CONNECTIONS) as executor:
-        q = queue.Queue(maxsize=MAX_QUEUE_SIZE)
-        overflow_list = []
+        #q = queue.Queue(maxsize=MAX_QUEUE_SIZE)
+        #overflow_list = []
 
         # Connect to up to NUM_CONNECTIONS servers
-        for i in range(NUM_CONNECTIONS):
+        #for i in range(NUM_CONNECTIONS):
+        while(True):
             answer = input('Would you like to connect (yes/no)? ')
             if answer.lower() != 'y':
                 break
@@ -59,47 +60,47 @@ def start():
                 continue
 
             # Add the client to the queue or the overflow list
-            try:
-                q.put(client_socket, block=True, timeout=QUEUE_WAIT_TIME)
-                logging.info("[QUEUEING] Client queued.")
-            except queue.Full:
+            #try:
+                #q.put(client_socket, block=True, timeout=QUEUE_WAIT_TIME)
+                #logging.info("[QUEUEING] Client queued.")
+            #except queue.Full:
                 # The queue is full, send the client to the overflow list
-                overflow_list.append(client_socket)
-                logging.warning("[QUEUE FULL] Client sent to overflow list.")
-                continue
+                #overflow_list.append(client_socket)
+                #logging.warning("[QUEUE FULL] Client sent to overflow list.")
+                #continue
 
             # Submit the client connection to a free thread
-            executor.submit(handle_client, client_socket, q)
+            executor.submit(handle_client, client_socket)
 
         # Wait for a certain amount of time before connecting to the next server
         time.sleep(1)
 
         # Process clients in the overflow list
-        while len(overflow_list) > 0:
+        #while len(overflow_list) > 0:
             # Check if the queue has capacity
-            if not q.full():
+            #if not q.full():
                 # Move clients from the overflow list to the end of the queue
-                client_socket = overflow_list.pop(0)
-                q.put(client_socket, block=True, timeout=QUEUE_WAIT_TIME)
-                logging.info("[QUEUEING] Client queued from overflow list.")
+                #client_socket = overflow_list.pop(0)
+                #q.put(client_socket, block=True, timeout=QUEUE_WAIT_TIME)
+                #logging.info("[QUEUEING] Client queued from overflow list.")
             # Wait for a certain amount of time before checking the queue again
-            time.sleep(5)
+            #time.sleep(5)
 
     # Remove the clients from the queue and the overflow list when done
-    try:
-        while True:
-            client_socket = q.get_nowait()
-            client_socket.close()
-            logging.info("[DISCONNECTED] Client disconnected.")
-    except queue.Empty:
-        pass
-    try:
-        while True:
-            client_socket = overflow_list.pop(0)
-            client_socket.close()
-            logging.info("[DISCONNECTED] Client disconnected from overflow list.")
-    except IndexError:
-        pass
+    #try:
+    #    while True:
+    #        client_socket = q.get_nowait()
+    #        client_socket.close()
+    #        logging.info("[DISCONNECTED] Client disconnected.")
+    #except queue.Empty:
+    #    pass
+    #try:
+    #    while True:
+    #        client_socket = overflow_list.pop(0)
+    #        client_socket.close()
+    #        logging.info("[DISCONNECTED] Client disconnected from overflow list.")
+    #except IndexError:
+    #    pass
 
 if __name__ == '__main__':
     start()
--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import socket
-import queue
 import logging
 import concurrent.futures
+import queue
 import time
+import threading
+from colorprints import ColorPrints
 
 # Server configuration
 SERVER_ADDRESS = ('localhost', 8000)
-MAX_QUEUE_SIZE = 10
-QUEUE_WAIT_TIME = 1
+MAX_CONNECTIONS = 5
+MAX_QUEUE_SIZE = 20
+OVERFLOW_QUEUE_SIZE = 20
+IDLE_TIMEOUT = 3  # Define the idle timeout in seconds
+OVERFLOW_CHECK_INTERVAL = 3  # Define the overflow check interval in seconds
 
 # Message configuration
 FORMAT = 'utf-8'
@@ -16,121 +21,137 @@ DISCONNECT_MESSAGE = '!DISCONNECT'
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-def handle_client(client_socket, client_addr, q, overflow_list):
-    #client_socket.settimeout(3)  # Set the timeout to 3 seconds
+# Set up the server socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(SERVER_ADDRESS)
+server_socket.listen(MAX_CONNECTIONS)
+logging.info(f"[LISTENING] Server is listening on {SERVER_ADDRESS}.")
 
+# Set up a queue to hold incoming clients
+client_queue = queue.Queue(maxsize=MAX_QUEUE_SIZE)
+
+# Set up an overflow queue to hold clients when the main queue is full
+overflow_queue = queue.Queue(maxsize=OVERFLOW_QUEUE_SIZE)
+
+
+def handle_client(client_socket, client_addr):
     logging.info(f"[NEW THREAD] Handling {client_addr}.")
 
-    # Add the client to the queue or the overflow list
-    try:
-        q.put(client_socket, block=True, timeout=QUEUE_WAIT_TIME)
-        logging.info(f"[QUEUEING] {client_addr} queued.")
-    except queue.Full:
-        # The queue is full, send the client to the overflow list
-        overflow_list.append(client_socket)
-        logging.warning(f"[QUEUE FULL] {client_addr} sent to overflow list.")
+    # Set a timeout on the client socket
+    client_socket.settimeout(IDLE_TIMEOUT)
 
-    # Receive messages from the client
-    connected = True
-    while connected:
+    # Loop to connect 4 times
+    for i in range(4):
+        # Receive messages from the client
+        connected = True
+        while connected:
+            try:
+                msg = client_socket.recv(1024).decode(FORMAT)
+            except socket.timeout:
+                # The client is idle, disconnect it
+                ColorPrints.print_in_red(f"[IDLE TIMEOUT] {client_addr} disconnected.")
+                connected = False
+                break
+
+            if len(msg) == 0:
+                # Connection was closed by the client
+                ColorPrints.print_in_cyan(f"[CONNECTION CLOSED] {client_addr} connection was closed.")
+                break
+
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+                # Disconnect from the client
+                client_socket.close()
+                ColorPrints.print_in_red(f"[DISCONNECTED] {client_addr} disconnected.")
+            else:
+                logging.info(f"[{client_addr}] {msg}")
+
+        # Remove the client from the queue
         try:
-            msg = client_socket.recv(1024).decode(FORMAT)
-        except socket.timeout:
-            # The client took too long to send a message
-            logging.warning(f"[TIMEOUT] {client_addr} took too long to send a message.")
-            break
-        except ConnectionResetError:
-            # Connection was reset by the client
-            logging.warning(f"[CONNECTION RESET] {client_addr} connection was reset.")
-            break
-
-        if len(msg) == 0:
-            # Connection was closed by the client
-            logging.info(f"[CONNECTION CLOSED] {client_addr} connection was closed.")
-            break
-
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
-            # Disconnect from the client
-            client_socket.close()
-            logging.info(f"[DISCONNECTED] {client_addr} disconnected.")
-        else:
-            logging.info(f"[{client_addr}] {msg}")
-
-    # Remove the client from the queue or the overflow list
-    try:
-        q.get_nowait()
-        logging.info(f"[DEQUEUEING] {client_addr} dequeued.")
-    except queue.Empty:
-        if client_socket in overflow_list:
-            overflow_list.remove(client_socket)
-            logging.info(f"[DEQUEUEING] {client_addr} dequeued from overflow list.")
-        else:
+            client_queue.get_nowait()
+            ColorPrints.print_in_yellow(f"[DEQUEUEING] {client_addr} dequeued.")
+        except queue.Empty:
             pass
 
-def handle_overflow_list(q, overflow_list):
+        # Check if there is space in the main queue
+        if not client_queue.full():
+            # Add the client to the main queue
+            try:
+                client_queue.put(client_socket, block=True, timeout=1.0)
+                ColorPrints.print_in_purple(f"[QUEUEING] {client_addr} queued.")
+            except queue.Full:
+                pass
+        else:
+            # Add the client to the overflow queue
+            try:
+                overflow_queue.put(client_socket, block=True, timeout=1.0)
+                logging.info(f"[OVERFLOW QUEUEING] {client_addr} queued in overflow queue.")
+            except queue.Full:
+                # The overflow queue is also full, reject the client
+                client_socket.send("Sorry, the server is busy. Please try again later.".encode(FORMAT))
+                client_socket.close()
+                logging.warning(f"[OVERFLOW QUEUE FULL] {client_addr} rejected.")
+
+        # Wait for a short time before connecting again
+        time.sleep(1)
+
+    logging.info(f"[FINISHED] {client_addr} finished connecting.")
+
+
+def check_overflow_queue():
     while True:
-        # Check if the queue has capacity
-        if not q.full():
-            # Move clients from the overflow list to the queue
-            while len(overflow_list) > 0:
-                client_socket = overflow_list.pop(0)
-                q.put(client_socket, block=True, timeout=QUEUE_WAIT_TIME)
-                logging.info(f"[QUEUEING] Client from overflow list queued.")
-        # Wait for a certain amount of time before checking the queue again
-        time.sleep(5)
+        # Check if there is space in the main queue
+        if not client_queue.full():
+            # Move clients from the overflow queue to the main queue
+            while not overflow_queue.empty():
+                client_socket = overflow_queue.get()
+                try:
+                    client_queue.put(client_socket, block=True, timeout=1.0)
+                    logging.info(f"[OVERFLOW DEQUEUEING] Client dequeued from overflow queue and queued in main queue.")
+                except queue.Full:
+                    # The main queue is full, put the client back in the overflow queue
+                    overflow_queue.put(client_socket)
+                    logging.info(f"[OVERFLOW REQUEUEING] Client put back in overflow queue.")
+                    break
+                
+        time.sleep(OVERFLOW_CHECK_INTERVAL)
+
 
 def start():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(SERVER_ADDRESS)
-    server.listen()
+    # Create a thread pool of MAX_CONNECTIONS threads
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_CONNECTIONS) as executor:
+        # Start the overflow queue checker
+        overflow_checker = threading.Thread(target=check_overflow_queue)
+        overflow_checker.daemon = True
+        overflow_checker.start()
 
-    # Create a thread pool of 5 threads
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        q = queue.Queue(maxsize=MAX_QUEUE_SIZE)
-        overflow_list = []
-        executor.submit(handle_overflow_list, q, overflow_list)
-
-        # Keep accepting client connections
+        # Accept clients and add them to the queue
         while True:
-            # Wait for a client to connect
             try:
-                client_socket, client_addr = server.accept()
-            except KeyboardInterrupt:
-                # Server was interrupted by the user
-                logging.info("[SERVER INTERRUPTED] Server was interrupted by the user.")
-                break
-            except socket.timeout:
-                # Timeout waiting for client connection
-                continue
+                client_socket, client_addr = server_socket.accept()
+                logging.info(f"[NEW CONNECTION] {client_addr} connected.")
             except Exception as e:
                 logging.error(f"[ERROR] {e}")
                 continue
 
-            logging.info(f"[NEW CONNECTION] {client_addr} connected.")
+            # Add the client to the queue
+            try:
+                client_queue.put(client_socket, block=True, timeout=1.0)
+                logging.info(f"[QUEUEING] {client_addr} queued.")
+            except queue.Full:
+                # The main queue is full, add the client to the overflow queue
+                try:
+                    overflow_queue.put(client_socket, block=True, timeout=1.0)
+                    logging.info(f"[OVERFLOW QUEUEING] {client_addr} queued in overflow queue.")
+                except queue.Full:
+                    # The overflow queue is also full, reject the client
+                    client_socket.send("Sorry, the server is busy. Please try again later.".encode(FORMAT))
+                    client_socket.close()
+                    logging.warning(f"[OVERFLOW QUEUE FULL] {client_addr} rejected.")
 
             # Submit the client connection to a free thread
-            executor.submit(handle_client, client_socket, client_addr, q, overflow_list)
+            executor.submit(handle_client, client_socket, client_addr)
 
-            # Print the current queue
-            #logging.info(f"[QUEUE] Current queue: {q.queue}")
-
-    # Remove the clients from the queue and the overflow list when done
-    try:
-        while True:
-            client_socket = q.get_nowait()
-            logging.info(f"[DEQUEUEING] Client dequeued.")
-    except queue.Empty:
-        pass
-    try:
-        while True:
-            client_socket = overflow_list.pop(0)
-            logging.info(f"[DEQUEUEING] Client dequeued from overflow list.")
-    except IndexError:
-        pass
-
-    # Close the server socket
-    server.close()
 
 if __name__ == '__main__':
     start()
